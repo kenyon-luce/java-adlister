2ffbe0274f47bddd063fca7c1c4620f627fa6440
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-26 11:27:32 -0500

    got results

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -4,7 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name="MarcoHelloWorld", urlPatterns = "/") //has to be outside the class. This is the metadata to go with our servlet
+@WebServlet(name="MarcoHelloWorld", urlPatterns = "/hello") //has to be outside the class. This is the metadata to go with our servlet
 public class HelloWorldServlet extends HttpServlet { //this class extends from HttpServlet
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
