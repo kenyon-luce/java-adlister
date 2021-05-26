d2b5e56e8deb9cf85298ed91d4603fb1ee6e947f
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-26 15:31:27 -0500

    added count

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,12 +5,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name="MarcoHelloWorld", urlPatterns = "/hello") //has to be outside the class. This is the metadata to go with our servlet
+@WebServlet(name="MarcoHelloWorld",
+        urlPatterns = "/hello") //has to be outside the class. This is the metadata to go with our servlet
 public class HelloWorldServlet extends HttpServlet { //this class extends from HttpServlet
     String name;
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        name = request.getParameter("name");
+    int count;
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        name = request.getParameter("name");
         if(name != null){
             response.getWriter().println("<h1>Hello, " + name + "!</h1>");
         } else {
@@ -18,5 +22,15 @@ public class HelloWorldServlet extends HttpServlet { //this class extends from H
         }
         //localhost:8080/hello?name=tim --> Hello, tim!
         //localhost:8080/hello?name=kenyon+luce --> Hello, kenyon luce!
+
+
+        //COUNT NUMBER OF VISITS + RESET
+        if(request.getParameter("count").equals("start")){
+            count++;
+        }
+        else if(request.getParameter("count").equals("reset")){
+            count = 0;
+        }
+        response.getWriter().println("Times visited: " + count);
     }
 }
