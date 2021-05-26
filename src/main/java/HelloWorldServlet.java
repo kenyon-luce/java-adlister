8c71b09d07a0bee24f439d25886e828a0b08715a
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-26 10:49:44 -0500

    servlet test

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,14 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+@WebServlet(name="MarcoHelloWorld", urlPatterns = "/") //has to be outside the class. This is the metadata to go with our servlet
+public class HelloWorldServlet extends HttpServlet { //this class extends from HttpServlet
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.getWriter().println("<h1>Hello World from the marco cohort!</h1>");
 
     }
 }
