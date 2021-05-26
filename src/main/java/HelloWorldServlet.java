bdca9dde96ac407bd0aab487f9bd46ffe10427ea
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-26 12:21:52 -0500

    added name

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -3,12 +3,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name="MarcoHelloWorld", urlPatterns = "/hello") //has to be outside the class. This is the metadata to go with our servlet
 public class HelloWorldServlet extends HttpServlet { //this class extends from HttpServlet
+    String name;
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        name = request.getParameter("name");
 
-        response.getWriter().println("<h1>Hello World from the marco cohort!</h1>");
-
+        if(name != null){
+            response.getWriter().println("<h1>Hello " + name + "!</h1>");
+        } else {
+            response.getWriter().println("<h1>Hello World!</h1>");
+        }
     }
 }
